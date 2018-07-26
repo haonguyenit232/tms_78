049331b42f80d2082ca42d6718383180a38275f9
haonguyenit232 <haonguyenit232@gmail.com>
2018-07-26 12:28:40 +0700

    subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,4 +4,25 @@ class Subject < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
   has_many :users, through: :user_subjects
+  accepts_nested_attributes_for :tasks, allow_destroy: true
+
+  mount_uploader :image_url, ImgSubjectUploader
+
+  validates :name, presence: true
+  validates :description, presence: true, length: {minimum: 10}
+  validate :validate_tasks
+
+  scope :recent, ->{order created_at: :desc}
+
+  def is_inprogess?
+    self.courses.any?{|course| course.started?}
+  end
+
+  private
+
+  def validate_tasks
+    if tasks.select{|task| !task._destroy}.count < Settings.task_quanlity
+      errors.add :subjects, I18n.t("admin.subjects.task_quanlity_error")
+    end
+  end
 end
