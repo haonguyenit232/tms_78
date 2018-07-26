8dc28d05d1284695dbdf5af09efb1abaede4299b
haonguyenit232 <haonguyenit232@gmail.com>
2018-07-26 12:45:49 +0700

    user_subject

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ApplicationRecord
   belongs_to :subject
   has_many :user_tasks, dependent: :destroy
   has_many :users, through: :user_tasks
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -4,5 +4,20 @@ class UserSubject < ApplicationRecord
   belongs_to :subject
   has_many :user_tasks, dependent: :destroy
 
-  enum status: %i(pending started finished)
+  enum status: {pending: 0, started: 1, finished: 2}
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  scope :find_by_subject,->user_id, subject_id, user_course_id do
+    find_by(user_id: user_id, subject_id: subject_id, user_course_id: user_course_id)
+  end
+
+  scope :count_status,->status, user_id do
+    where(status: status, user_id: user_id).count(:status)
+  end
+
+  scope :count_subject,->user_id do
+    where(user_id: user_id).count(:subject_id)
+  end
 end
